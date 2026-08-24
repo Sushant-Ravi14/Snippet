9808bff4219397901c52666ec7abcf0267d17c53
Sushant-Ravi14 <sushant.ravi.cg@gmail.com>
2026-08-24 15:00:00 +0530

    Create User database model

---
+++ backend/models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default: 'default-avatar.png',
+    },
+    bio: {
+      type: String,
+      default: '',
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('User', userSchema);
