e05a6abf658c19eb03105863986a0bc2df7939e4
Sushant-Ravi14 <sushant.ravi.cg@gmail.com>
2026-08-25 15:00:00 +0530

    Create Post database model

---
+++ backend/models/Post.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    text: {
+      type: String,
+      maxlength: 500,
+    },
+    image: {
+      type: String, // path to image
+    },
+    location: {
+      locality: String,
+      latitude: Number,
+      longitude: Number,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Post', postSchema);
