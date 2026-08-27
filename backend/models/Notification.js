5bc6a3575d7ddddc0a415a8f4b25027b5fb7106b
Sushant-Ravi14 <sushant.ravi.cg@gmail.com>
2026-08-27 18:00:00 +0530

    Create Notification database model

---
+++ backend/models/Notification.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema(
+  {
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    actor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ['follow', 'like', 'comment'],
+      required: true,
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post',
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Notification', notificationSchema);
